ac305a9e3f7117b26781272f69b21e7583375774
riyad5541 <abdullahalriyad5541@gmail.com>
2022-11-25 20:41:59 +0600

    add signup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,28 +3,39 @@ import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom';
 
 const Login = () => {
-    const { register, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit } = useForm()
     
 
     const handleLogin = data => {
         console.log(data);
+        
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
-                <h2 className='text-2xl'>Login</h2>
+                <h2 className='text-2xl text-center'>Login</h2>
                 <form onSubmit={handleSubmit(handleLogin)}>
 
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
-                        <input type="text" {...register("email")} className="input input-bordered w-full max-w-xs" />
+                        <input type="text" {...register("email",{
+                                required: "Email Address is required"
+                            })} 
+                            className="input input-bordered w-full max-w-xs" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
-                        <input type="password" {...register("password")} className="input input-bordered w-full max-w-xs" />
+                        <input type="password" {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
                         <label className="label"><span className="label-text">Forget Password?</span></label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
                     <input className='btn btn-accent w-full' value="Login" type="submit" />
+                    
                 </form>
                 <p>New to Resale Books <Link className='text-primary' to="/signup">Create New Account</Link></p>
                 <div className="divider">OR</div>
